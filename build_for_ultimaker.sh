7d49a232ddfe0c6d0661dfd5f622804c1e76ce5f
Olliver Schinagl <oliver@schinagl.nl>
2018-07-04 16:06:57 +0200

    build: Add build scripts for manual builds
    Add a few scripts to make it easier to manually build.
    
    Contributes to issue EMP-109
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

---
+++ build_for_ultimaker.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: AGPL-3.0+
+#
+# Copyright (C) 2018 Ultimaker B.V.
+# Copyright (C) 2018 Olliver Schinagl <oliver@schinagl.nl>
+#
+
+ARCH="${ARCH:-armhf}"
+CI_REGISTRY_IMAGE="${CI_REGISTRY_IMAGE:-registry.gitlab.com/ultimaker/embedded/platform/ulticontroller_interface_tester}"
+CI_REGISTRY_IMAGE_TAG="${CI_REGISTRY_IMAGE_TAG:-latest}"
+CROSS_COMPILE="${CROSS_COMPILE:-arm-linux-gnueabihf-}"
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH:-/usr/lib/arm-linux-gnueabihf/pkgconfig/}"
+WORK_DIR="${WORK_DIR:-/workdir}"
+
+set -eu
+
+run_in_docker()
+{
+    docker run --rm -i -t -h "$(hostname)" -u "$(id -u):$(id -g)" \
+        -e "ARCH=${ARCH}" \
+        -e "CROSS_COMPILE=${CROSS_COMPILE}" \
+        -e "PKG_CONFIG_PATH=${PKG_CONFIG_PATH}" \
+        -e "MAKEFLAGS=-j$(($(getconf _NPROCESSORS_ONLN) - 1))" \
+        -v "$(pwd):${WORK_DIR}" \
+        -w "${WORK_DIR}" \
+        "${CI_REGISTRY_IMAGE}:${CI_REGISTRY_IMAGE_TAG}" \
+        "${@}"
+}
+
+run_in_shell()
+{
+    PKG_CONFIG_PATH="${PKG_CONFIG_PATH}"
+    CROSS_COMPILE="${CROSS_COMPILE}" \
+        sh "${@}"
+}
+
+run_script()
+{
+    if ! command -V docker; then
+        echo "Docker not found, attempting native run."
+
+        run_in_shell "${@}"
+    else
+        run_in_docker "${@}"
+    fi
+}
+
+build()
+{
+    echo "Starting build"
+    run_script "./build.sh" "${@}"
+    echo "Build complete"
+}
+
+env_check()
+{
+    echo "Checking environment support."
+    run_script "./test/buildenv.sh"
+}
+
+env_check
+build "${@}"
+
+exit 0

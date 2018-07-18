2d2411423279a617b0c26e4d05ab6e656f2af8be
Olliver Schinagl <oliver@schinagl.nl>
2018-07-18 15:28:40 +0200

    ci: Add initial ci configuration
    Add initial untested CI files needed to run in gitlab-ci.
    
    Contributes to issue EMP-109
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

--- test/buildenv.sh
+++ test/buildenv.sh
@@ -1,21 +1,60 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
 TEST_DIR=$(mktemp -d)
 
-cleanup() {
-       if [ "$(dirname "${TEST_DIR}")" != "/tmp" ]; then
-               exit 1
-       fi
-       rm -rf "${TEST_DIR}"
+test_failed=false
+
+cleanup()
+{
+    if [ "$(dirname "${TEST_DIR}")" != "/tmp" ]; then
+        exit 1
+    fi
+    rm -rf "${TEST_DIR}"
 }
 
 trap cleanup EXIT
 
-echo "int main(void) { return 0; }" > "${TEST_DIR}/compiler_test.c"
+check_compiler()
+{
+    cat <<-EOT > "${TEST_DIR}/compiler_test.c"
+	#include <libevdev/libevdev.h>
 
-"${CROSS_COMPILE}gcc" -o "${TEST_DIR}/compiler_test.o" -c "${TEST_DIR}/compiler_test.c" -Wall -Werror
-"${CROSS_COMPILE}objdump" -dS "${TEST_DIR}/compiler_test.o" 1> /dev/null
+	int main(void)
+	{
+	    struct libevdev *dev = libevdev_new();
+
+	    libevdev_free(dev);
+	    return 0;
+	}
+EOT
+
+    # shellcheck disable=SC2046
+    # allow word splitting for pkg-config
+    "${CROSS_COMPILE}gcc" \
+        $(pkg-config --libs --cflags libevdev) \
+        -o "${TEST_DIR}/compiler_test" \
+        -c "${TEST_DIR}/compiler_test.c" \
+        -Wall -Werror || test_failed=true
+
+    "${CROSS_COMPILE}objdump" -dS "${TEST_DIR}/compiler_test" 1> /dev/null || test_failed=true
+
+    printf "compiler and libevdev support: "
+    if "${test_failed}"; then
+        echo "error"
+    else
+      echo "ok"
+    fi
+}
+
+check_compiler
+
+if "${test_failed}"; then
+   echo "ERROR: Missing preconditions, cannot continue."
+   exit 1
+fi
+
+echo "All Ok"
 
 exit 0
